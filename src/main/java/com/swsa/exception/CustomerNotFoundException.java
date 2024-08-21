5b0e92b1f5528b94a149c3e0b592c7aac6a13d05
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-08-21 11:06:31 +0530

    Committed WebUI

--- src/main/java/com/swsa/exception/CustomerNotFoundException.java
+++ src/main/java/com/swsa/exception/CustomerNotFoundException.java
@@ -1,8 +1,8 @@
 package com.swsa.exception;
 
     public  class CustomerNotFoundException  extends RuntimeException {
-        public CustomerNotFoundException (long aadhaarNo) {
-            super("aadhaarNo Not Found " + aadhaarNo);
+        public CustomerNotFoundException (int customerId) {
+            super("customerId Not Found " + customerId);
         }
     }
 
